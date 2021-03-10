e014ad67e825bbdae997cb26b6c36e6f412ac76a
repl.it user <replituser@example.com>
2021-03-10 21:00:43 +0000

    Task 1-3

--- lab3/src/find_min_max.c
+++ lab3/src/find_min_max.c
@@ -2,11 +2,25 @@
 
 #include <limits.h>
 
-struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) {
-  struct MinMax min_max;
-  min_max.min = INT_MAX;
-  min_max.max = INT_MIN;
+struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end)
+{
+      struct MinMax min_max;
+      min_max.min = INT_MAX;
+      min_max.max = INT_MIN;
 
-  // your code here
-  return min_max;
+      int min = array[begin], max = array[begin];
+      
+      for (int i = begin; i < end; i++)
+      {
+          if (array[i]<min) {
+            min=array[i];
+          }              
+          if (array[i]>max) {
+            max=array[i];
+          }
+      }
+
+      min_max.min=min;
+      min_max.max=max;
+      return min_max;
 }
